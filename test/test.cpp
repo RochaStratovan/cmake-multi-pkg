d3119ff2e2c11d257a12969e3c4171dbe84422c2
John Rocha <Rocha@Stratovan.com>
2024-09-23 12:19:55 -0700

    Add test executable

---
+++ test/test.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <foo1.h>
+#include <foo2.h>
+#include <bar1.h>
+#include <bar2.h>
+
+
+
+
+
+int main(void)
+{
+    printf("This is the test exe\n");
+    foo1();
+    foo2();
+    bar1();
+    bar2();
+}
