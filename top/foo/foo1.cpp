07bce13972559748243d1a7e6f15f2a39e34906e
John Rocha <Rocha@Stratovan.com>
2024-09-23 12:18:53 -0700

    Add foo library

---
+++ top/foo/foo1.cpp
@@ -0,0 +1,9 @@
+#include "foo1.h"
+#include <stdio.h>
+
+
+
+void foo1(void)
+{
+    printf("Hello from foo1\n");
+}
---
+++ top/foo/foo2.cpp
@@ -0,0 +1,9 @@
+#include "foo2.h"
+#include <stdio.h>
+
+
+
+void foo2(void)
+{
+    printf("Hello from foo2\n");
+}
