79d1558b95d21dc423ec00a57b1b9a770694f482
John Rocha <Rocha@Stratovan.com>
2024-09-23 12:19:03 -0700

    Add bar library

---
+++ top/bar/bar1.cpp
@@ -0,0 +1,9 @@
+#include "bar1.h"
+#include <stdio.h>
+
+
+
+void bar1(void)
+{
+    printf("Hello from bar1\n");
+}
---
+++ top/bar/bar2.cpp
@@ -0,0 +1,9 @@
+#include "bar2.h"
+#include <stdio.h>
+
+
+
+void bar2(void)
+{
+    printf("Hello from bar2\n");
+}
